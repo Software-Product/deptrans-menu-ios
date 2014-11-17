63937fa87358c440bfcd68ddcee2750d17d66829
Stas Batururimi <blueocean87@me.com>
2014-11-17 19:35:20 +0300

    New lib version:Possibility to show status bar wherever you want added (enjoy :) )

--- Library/DeptransMenu.h
+++ Library/DeptransMenu.h
@@ -37,8 +37,15 @@
 /// установка другого шрифта, отличного от HelveticaNeue. Задается fontName, без размера.
 + (void) setDefaultFont:(NSString *)regularFont lightFont:(NSString *)lightFont;
 
- /// управление анимацией, ее скоростью, углом поворота и высотой остающейся снизу части. Значения по умолчанию — 0.35, 38, 0.8
+/// управление анимацией, ее скоростью, углом поворота и высотой остающейся снизу части. Значения по умолчанию — 0.35, 38, 0.8
 + (void) setPerspectiveAnimationDuration:(float)seconds angle:(float)degrees scale:(CGFloat)scale;
+
+/// включает или отключает отображение статус-бара на скриншоте хост-приложения, который используется в анимации открытия меню
++ (void) setShowsStatusBarOnHostApplication:(BOOL)flag;
+
+/// включает или отключает отображение статус-бара в самом меню, когда оно открыто
++ (void) setShowsStatusBarWhenMenuIsOpened:(BOOL)flag;
+
 
 @end
 
